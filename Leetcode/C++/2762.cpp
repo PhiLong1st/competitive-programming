42621cc773b189e2b04642c7d7fb843bf3b06937
PhiLong1st <philongnguyen1704@gmail.com>
2025-05-08 22:59:23 +0700

    Add Leetcode

---
+++ Leetcode/C++/2762.cpp
@@ -0,0 +1,23 @@
+
+/*
+    Code by: KoKoDuDu
+    Created: 2025.05.08 22:59:10
+*/
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        const int n = nums.size();
+        long long cnt = 0;
+        multiset<int> hasX;
+        for (int l = 0, r = 0; r < n; r++) {
+            hasX.insert(nums[r]);
+            while (l < r && *prev(hasX.end()) - *hasX.begin() > 2) {
+                auto it = hasX.find(nums[l]);
+                hasX.erase(it);
+                l++;
+            }
+            cnt += r - l + 1LL;
+        }
+        return cnt;
+    }
+};
