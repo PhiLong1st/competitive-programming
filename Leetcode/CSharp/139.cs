e6dd64d143917de6054af36f1cb581191fa7a39a
PhiLong1st <philongnguyen17048@gmail.com>
2025-08-04 14:04:08 +0700

    Add Leetcode

---
+++ Leetcode/CSharp/139.cs
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public bool WordBreak(string s, IList<string> wordDist)
+    {
+        int n = s.Length;
+        var wordSet = new HashSet<string>(wordDist);
+        bool[] dp = new bool[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (dp[j] && wordSet.Contains(s.Substring(j, i - j)))
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
