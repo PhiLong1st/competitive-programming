bbcb0c6d374f160955e94d7f69250a86c74856c3
PhiLong1st <philongnguyen17048@gmail.com>
2025-08-26 11:01:11 +0700

    feat: add codeforces 105911A

---
+++ Codeforces/105911A.cpp
@@ -0,0 +1,31 @@
+/*
+    Code by: KoKoDuDu
+    Created: 2025.08.26 11:00:47
+*/
+
+#include <bits/stdc++.h>
+#define int long long
+#define pll pair<int, int>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+void solve() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  cout << d * (a + b + c) << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
