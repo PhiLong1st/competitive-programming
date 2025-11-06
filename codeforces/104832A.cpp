88535c8be6d553728a4569f4256e1afe503f75e9
PhiLong1st <philongnguyen1704@gmail.com>
2025-11-06 17:05:56 +0700

    feat: refactor solution for Codeforces problem 104832A using dynamic programming

--- codeforces/104832A.cpp
+++ codeforces/104832A.cpp
@@ -17,14 +17,55 @@ const int kMod = 1e9 + 7;
 const int kMaxInf = numeric_limits<int>::max();
 const int kMinInf = numeric_limits<int>::min();
 
-const int dx[4] = {0, 0, -1, 1};
-const int dy[4] = {-1, 1, 0, 0};
+const int dr[4] = {0, 0, -1, 1};
+const int dc[4] = {-1, 1, 0, 0};
 
 #ifdef ONLINE_JUDGE
 #define dbg(...)
 #else
 #include "debug.h"
 #endif
+
+// void solve() {
+//   int n, m;
+//   cin >> n >> m;
+
+//   vector<string> a(n);
+//   for (auto& s : a) cin >> s;
+
+//   string str = "YOKOHAMA";
+//   int sz = str.size();
+
+//   int ans = 0;
+
+// auto in_bounds = [&](int r, int c) -> bool {
+//   return 0 <= r && r < n && 0 <= c && c < m;
+// };
+
+//   auto dfs = [&](auto&& dfs, int r, int c, int p) -> void {
+//     if (p == sz - 1) {
+//       ans++;
+//       return;
+//     }
+
+//     for (int i = 0; i < 4; ++i) {
+//       int nr = r + dr[i];
+//       int nc = c + dc[i];
+//       int np = p + 1;
+
+//       if (in_bounds(nr, nc) && a[nr][nc] == str[np]) dfs(dfs, nr, nc, np);
+//     }
+//   };
+
+//   for (int i = 0; i < n; ++i) {
+//     for (int j = 0; j < m; ++j) {
+//       if (a[i][j] != 'Y') continue;
+//       dfs(dfs, i, j, 0);
+//     }
+//   }
+
+//   cout << ans;
+// }
 
 void solve() {
   int n, m;
@@ -33,37 +74,44 @@ void solve() {
   vector<string> a(n);
   for (auto& s : a) cin >> s;
 
-  string str = "YOKOHAMA";
-  int sz = str.size();
-
-  int ans = 0;
-
   auto in_bounds = [&](int r, int c) -> bool {
     return 0 <= r && r < n && 0 <= c && c < m;
   };
 
-  auto dfs = [&](auto&& dfs, int r, int c, int p) -> void {
-    if (p == sz - 1) {
-      ans++;
-      return;
-    }
+  string str = "YOKOHAMA";
+  int sz = str.size();
 
-    for (int i = 0; i < 4; ++i) {
-      int nr = r + dx[i];
-      int nc = c + dy[i];
-      int np = p + 1;
-
-      if (in_bounds(nr, nc) && a[nr][nc] == str[np]) dfs(dfs, nr, nc, np);
-    }
-  };
-
+  vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(sz)));
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < m; ++j) {
       if (a[i][j] != 'Y') continue;
-      dfs(dfs, i, j, 0);
+      dp[i][j][0] = 1;
     }
   }
 
+  for (int p = 1; p < sz; ++p) {
+    for (int r = 0; r < n; ++r) {
+      for (int c = 0; c < m; ++c) {
+        if (a[r][c] != str[p]) {
+          dp[r][c][p] = 0;
+          continue;
+        }
+
+        for (int k = 0; k < 4; ++k) {
+          int nr = r + dr[k], nc = c + dc[k];
+          if (!in_bounds(nr, nc)) continue;
+          dp[r][c][p] += dp[nr][nc][p - 1];
+        }
+      }
+    }
+  }
+
+  int ans = 0;
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      ans += dp[i][j][sz - 1];
+    }
+  }
   cout << ans;
 }
 
