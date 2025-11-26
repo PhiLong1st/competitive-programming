d053706eb75439ae55ce3c0584bb6d78cf453a11
PhiLong1st <philongnguyen1704@gmail.com>
2025-11-26 23:58:49 +0700

    feat: add implementations for Codeforces Educational Round 164 problems A to D

---
+++ codeforces/Educational_Codeforces_Round_164/1954D.cpp
@@ -0,0 +1,92 @@
+/*
+ Code by: KoKoDuDu
+ Created: 2025.11.26 10:18:39
+*/
+
+#include <bits/stdc++.h>
+#define int long long
+#define ldb long double
+#define pii pair<int, int>
+#define cd complex<double>
+#define fi first
+#define se second
+
+using namespace std;
+
+const double PI = acos(-1);
+const int kMaxN = 2e5 + 0307;
+const int kMod = 998244353;
+const int kMaxBit = 60;
+const int kMaxInf = 1e18;
+const int kMinInf = -1e18;
+
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include "test/debug.h"
+#endif
+
+int ceil_div(int a, int b) { return a >= 0 ? (a + b - 1) / b : a / b; }
+
+int floor_div(int a, int b) { return a <= 0 ? (a - b + 1) / b : a / b; }
+
+void add(int& a, int b) { a = (a + b >= kMod) ? a + b - kMod : a + b; }
+
+int minus_mod(int a, int b) { return (a - b + kMod) % kMod; }
+
+int mul_mod(int a, int b) { return a * b % kMod; }
+
+void maximize(int& x, int y) { x = max(x, y); }
+
+void minimize(int& x, int y) { x = min(x, y); }
+
+int fpow(int a, int b) {
+  int res = 1;
+  while (b) {
+    if (b & 1) res = res * a % kMod;
+    a = a * a % kMod;
+    b >>= 1;
+  }
+  return res;
+}
+
+int gcd(int a, int b) {
+  if (a < b) swap(a, b);
+  return b == 0 ? a : gcd(b, a % b);
+}
+
+void solve() {
+  int n;
+  cin >> n;
+
+  vector<int> a(n + 1);
+  for (int i = 1; i <= n; ++i) cin >> a[i];
+
+  sort(a.begin() + 1, a.end());
+
+  int ans = 0;
+  int total = accumulate(a.begin() + 1, a.end(), 0LL);
+
+  vector<int> dp(total + 1);
+  dp[0] = 1;
+  for (int i = 1; i <= n; ++i) {
+    for (int sum = 0; sum <= total; ++sum) {
+      if (sum <= a[i])
+        add(ans, dp[sum] * a[i] % kMod);
+      else
+        add(ans, ((sum + 1 + a[i]) / 2) * dp[sum] % kMod);
+    }
+    for (int sum = total - a[i]; sum >= 0; --sum) add(dp[sum + a[i]], dp[sum]);
+  }
+  cout << ans;
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+  while (t--) solve();
+  return 0;
+}
