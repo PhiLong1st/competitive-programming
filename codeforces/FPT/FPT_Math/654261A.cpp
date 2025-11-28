435fc8f184a591e1380f4f818302283272cdcb1d
PhiLong1st <philongnguyen1704@gmail.com>
2025-11-28 14:09:18 +0700

    feat: add implementations for Codeforces problems 654261A and 654261F

---
+++ codeforces/FPT/FPT_Math/654261A.cpp
@@ -0,0 +1,77 @@
+/*
+ Code by: KoKoDuDu
+ Created: 2025.11.27 19:25:52
+*/
+
+#include <bits/stdc++.h>
+#define int long long
+#define ldb long double
+#define pii pair<int, int>
+#define cd complex<double>
+#define fi first
+#define se second
+
+using namespace std;
+
+const double PI = acos(-1);
+const int kMaxN = 2e5 + 0307;
+const int kMod = 1e9 + 7;
+const int kMaxBit = 60;
+const int kMaxInf = 1e18;
+const int kMinInf = -1e18;
+
+int ceil_div(int a, int b) { return a >= 0 ? (a + b - 1) / b : a / b; }
+
+int floor_div(int a, int b) { return a <= 0 ? (a - b + 1) / b : a / b; }
+
+int add_mod(int a, int b) { return a + b >= kMod ? a + b - kMod : a + b; }
+
+int minus_mod(int a, int b) { return (a - b + kMod) % kMod; }
+
+int mul_mod(int a, int b) { return a * b % kMod; }
+
+void maximize(int& x, int y) { x = max(x, y); }
+
+void minimize(int& x, int y) { x = min(x, y); }
+
+int fpow(int a, int b) {
+  int res = 1;
+  while (b) {
+    if (b & 1) res = res * a % kMod;
+    a = a * a % kMod;
+    b >>= 1;
+  }
+  return res;
+}
+
+int gcd(int a, int b) {
+  if (a < b) swap(a, b);
+  return b == 0 ? a : gcd(b, a % b);
+}
+
+int calc(int l, int r, int k) {
+  if (l > r) return 0;
+  return r / k - (l - 1) / k;
+}
+
+void solve() {
+  int k, l, r;
+  cin >> k >> l >> r;
+
+  if (r < 0) l = abs(l), r = abs(r), swap(l, r);
+  if (l > 0) return void(cout << calc(l, r, k));
+
+  int s1 = calc(1, abs(l), k);
+  int s2 = calc(1, r, k);
+  cout << s1 + s2 + 1;
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+  while (t--) solve();
+  return 0;
+}
