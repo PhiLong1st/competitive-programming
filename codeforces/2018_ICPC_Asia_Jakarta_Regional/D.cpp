b0a16fe4ee42ca5327241e7f57001b70221b1504
PhiLong1st <philongnguyen1704@gmail.com>
2025-11-29 17:32:09 +0700

    feat: add implementation for Codeforces problem D from 2018 ICPC Asia Jakarta Regional

---
+++ codeforces/2018_ICPC_Asia_Jakarta_Regional/D.cpp
@@ -0,0 +1,115 @@
+/*
+ Code by: KoKoDuDu
+ Created: 2025.11.20 17:39:24
+*/
+
+#include <bits/stdc++.h>
+#define int long long
+#define ldb long double
+#define pii pair<int, int>
+#define cd complex<double>
+#define fi first
+#define se second
+
+using namespace std;
+
+const double PI = acos(-1);
+const int kMaxN = 2e5 + 0307;
+const int kMod = 1e9 + 7;
+const int kMaxBit = 60;
+const int kMaxInf = 1e18;
+const int kMinInf = -1e18;
+
+int ceil_div(int a, int b) { return a >= 0 ? (a + b - 1) / b : a / b; }
+
+int floor_div(int a, int b) { return a <= 0 ? (a - b + 1) / b : a / b; }
+
+int add_mod(int a, int b) { return a + b >= kMod ? a + b - kMod : a + b; }
+
+int minus_mod(int a, int b) { return (a - b + kMod) % kMod; }
+
+int mul_mod(int a, int b) { return a * b % kMod; }
+
+void maximize(int& x, int y) { x = max(x, y); }
+
+void minimize(int& x, int y) { x = min(x, y); }
+
+int fpow(int a, int b) {
+  int res = 1;
+  while (b) {
+    if (b & 1) res = res * a % kMod;
+    a = a * a % kMod;
+    b >>= 1;
+  }
+  return res;
+}
+
+int gcd(int a, int b) {
+  if (a < b) swap(a, b);
+  return b == 0 ? a : gcd(b, a % b);
+}
+
+void solve() {
+  int n, m;
+  int ans = 0;
+
+  cin >> n >> m;
+  vector<string> a(n + 1);
+  for (int i = 1; i <= n; ++i) {
+    cin >> a[i];
+    a[i] = " " + a[i];
+  }
+
+  if (n == 1) {
+    for (int i = 2; i < m; ++i) ans += a[1][i] == '.';
+    cout << ans;
+    return;
+  }
+
+  if (m == 1) {
+    for (int i = 2; i < n; ++i) ans += a[i][1] == '.';
+    cout << ans;
+    return;
+  }
+
+  if (n == 2) {
+    for (int i = 2; i <= m - 1; ++i) ans += (a[1][i] == '.' && a[2][i] == '.');
+    cout << ans;
+    return;
+  }
+
+  if (m == 2) {
+    for (int i = 2; i <= n - 1; ++i) ans += (a[i][1] == '.' && a[i][2] == '.');
+    cout << ans;
+    return;
+  }
+
+  for (int i = 2; i <= n - 1; ++i) {
+    for (int j = 2; j <= m - 1; ++j) {
+      ans += a[i][j] == '.';
+    }
+  }
+
+  int is_valid = true;
+  for (int i = 2; i <= m - 1; ++i) {
+    is_valid &= a[1][i] == '.';
+    is_valid &= a[n][i] == '.';
+  }
+
+  for (int i = 2; i <= n - 1; ++i) {
+    is_valid &= a[i][1] == '.';
+    is_valid &= a[i][m] == '.';
+  }
+
+  cout << ans + is_valid;
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+  while (t--) solve();
+  return 0;
+}
