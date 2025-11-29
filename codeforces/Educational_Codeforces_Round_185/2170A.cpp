b74b8538e05267c84f3f6d88ed8c3f5733f124a6
PhiLong1st <philongnguyen1704@gmail.com>
2025-11-29 17:18:18 +0700

    feat: add implementations for Codeforces Educational Round 185 problems A to C

---
+++ codeforces/Educational_Codeforces_Round_185/2170A.cpp
@@ -0,0 +1,84 @@
+/*
+ Code by: KoKoDuDu
+ Created: 2025.11.28 21:35:59
+*/
+
+#include <bits/stdc++.h>
+#define int long long
+#define ldb long double
+#define pii pair<int, int>
+#define cd complex<double>
+#define fi first
+#define se second
+
+using namespace std;
+
+const double PI = acos(-1);
+const int kMaxN = 2e5 + 0307;
+const int kMod = 1e9 + 7;
+const int kMaxBit = 60;
+const int kMaxInf = 1e18;
+const int kMinInf = -1e18;
+
+int ceil_div(int a, int b) { return a >= 0 ? (a + b - 1) / b : a / b; }
+
+int floor_div(int a, int b) { return a <= 0 ? (a - b + 1) / b : a / b; }
+
+int add_mod(int a, int b) { return a + b >= kMod ? a + b - kMod : a + b; }
+
+int minus_mod(int a, int b) { return (a - b + kMod) % kMod; }
+
+int mul_mod(int a, int b) { return a * b % kMod; }
+
+void maximize(int& x, int y) { x = max(x, y); }
+
+void minimize(int& x, int y) { x = min(x, y); }
+
+int fpow(int a, int b) {
+  int res = 1;
+  while (b) {
+    if (b & 1) res = res * a % kMod;
+    a = a * a % kMod;
+    b >>= 1;
+  }
+  return res;
+}
+
+int gcd(int a, int b) {
+  if (a < b) swap(a, b);
+  return b == 0 ? a : gcd(b, a % b);
+}
+
+void solve() {
+  int n;
+  cin >> n;
+
+  vector<vector<int>> a(n + 2, vector<int>(n + 2));
+  int cnt = 0;
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      a[i][j] = ++cnt;
+    }
+  }
+
+  int ans = 0;
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      ans = max(
+          ans, a[i][j] + a[i][j - 1] + a[i - 1][j] + a[i][j + 1] + a[i + 1][j]);
+    }
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
---
+++ codeforces/Educational_Codeforces_Round_185/2170B.cpp
@@ -0,0 +1,93 @@
+/*
+ Code by: KoKoDuDu
+ Created: 2025.11.28 21:40:32
+*/
+
+#include <bits/stdc++.h>
+#define int long long
+#define ldb long double
+#define pii pair<int, int>
+#define cd complex<double>
+#define fi first
+#define se second
+
+using namespace std;
+
+const double PI = acos(-1);
+const int kMaxN = 2e5 + 0307;
+const int kMod = 1e9 + 7;
+const int kMaxBit = 60;
+const int kMaxInf = 1e18;
+const int kMinInf = -1e18;
+
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include "debug.h"
+#endif
+
+int ceil_div(int a, int b) { return a >= 0 ? (a + b - 1) / b : a / b; }
+
+int floor_div(int a, int b) { return a <= 0 ? (a - b + 1) / b : a / b; }
+
+int add_mod(int a, int b) { return a + b >= kMod ? a + b - kMod : a + b; }
+
+int minus_mod(int a, int b) { return (a - b + kMod) % kMod; }
+
+int mul_mod(int a, int b) { return a * b % kMod; }
+
+void maximize(int& x, int y) { x = max(x, y); }
+
+void minimize(int& x, int y) { x = min(x, y); }
+
+int fpow(int a, int b) {
+  int res = 1;
+  while (b) {
+    if (b & 1) res = res * a % kMod;
+    a = a * a % kMod;
+    b >>= 1;
+  }
+  return res;
+}
+
+int gcd(int a, int b) {
+  if (a < b) swap(a, b);
+  return b == 0 ? a : gcd(b, a % b);
+}
+
+void solve() {
+  int n;
+  cin >> n;
+
+  vector<int> a(n);
+  for (int& x : a) cin >> x;
+
+  int ans = 0;
+  sort(a.begin(), a.end());
+
+  int sum = accumulate(a.begin(), a.end(), 0LL);
+  int pf = 0;
+  int st = -1;
+
+  for (int i = 0; i < n; ++i) {
+    pf += a[i] != 0;
+
+    if (a[i] == 0) continue;
+    if (st == -1) st = i;
+
+    if (sum - pf >= n - 1) ans = i - st + 1;
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
