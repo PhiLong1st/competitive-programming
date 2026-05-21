f136d55a510644a1bbd0ff73c331679aed697858
PhiLong1st <philongnguyen1704@gmail.com>
2026-05-21 21:32:31 +0700

    feat: add solutions for VNOI CUP 2026 problems A, B, and C

---
+++ vnoj/VNOI_CUP_2026/A.cpp
@@ -0,0 +1,85 @@
+/*
+ Code by: KoKoDuDu
+ Created: 2026.05.16 20:27:20
+*/
+
+#include <bits/stdc++.h>
+#define int long long
+#define ldb long double
+#define pii pair<int, int>
+#define cd complex<double>
+#define fi first
+#define se second
+
+using namespace std;
+
+const double PI = acos(-1);
+const int kMaxN = 2e5 + 0307;
+const int kMod = 1e9 + 7;
+const int kMaxBit = 60;
+const int kMaxInf = 1e18;
+const int kMinInf = -1e18;
+
+int ceil_div(int a, int b) { return a >= 0 ? (a + b - 1) / b : a / b; }
+
+int floor_div(int a, int b) { return a <= 0 ? (a - b + 1) / b : a / b; }
+
+void add_mod(int& a, int b) { a = a + b >= kMod ? a + b - kMod : a + b; }
+
+void minus_mod(int& a, int b) { a = (a - b + kMod) % kMod; }
+
+void mul_mod(int& a, int b) { a = a * b % kMod; }
+
+void maximize(int& x, int y) { x = max(x, y); }
+
+void minimize(int& x, int y) { x = min(x, y); }
+
+int fpow(int a, int b) {
+  int res = 1;
+  while (b) {
+    if (b & 1) res = res * a % kMod;
+    a = a * a % kMod;
+    b >>= 1;
+  }
+  return res;
+}
+
+int gcd(int a, int b) {
+  if (a < b) swap(a, b);
+  return b == 0 ? a : gcd(b, a % b);
+}
+
+void solve() {
+  int n, m, r, c;
+  cin >> n >> m;
+  cin >> r >> c;
+
+  map<int, int> mpR, mpC;
+  int cntR = 0, cntC = 0;
+  int dr = 1, dc = 1, res = 0;
+
+  while (cntR != n && cntC != m) {
+    if (r == n) dr = -dr;
+    if (c == m) dc = -dc;
+
+    cntR += mpR[r] == 0;
+    cntC += mpC[c] == 0;
+
+    mpR[r] = 1, mpC[c] = 1;
+
+    r += dr, c += dc;
+    res++;
+  }
+  cout << res - 1 << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
