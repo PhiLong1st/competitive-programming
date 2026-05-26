4c1fc6ffaf06cde5bc5bfd130fb1e73d10d83162
PhiLong1st <philongnguyen1704@gmail.com>
2026-05-26 12:52:11 +0700

    feat: add solutions for Codeforces Round 1099 problems A, B, and C

---
+++ codeforces/Codeforces_Round_1099/2231C.cpp
@@ -0,0 +1,122 @@
+/*
+ Code by: KoKoDuDu
+ Created: 2026.05.26 12:36:24
+*/
+
+#include <bits/stdc++.h>
+#define int long long
+#define ldb long double
+#define pii pair<int, int>
+#define cd complex<double>
+#define fi first
+#define se second
+
+using namespace std;
+
+const double PI = acos(-1);
+const int kMaxN = 2e5 + 0307;
+const int kMod = 1e9 + 7;
+const int kMaxBit = 60;
+const int kMaxInf = 1e18;
+const int kMinInf = -1e18;
+
+int ceil_div(int a, int b) { return a >= 0 ? (a + b - 1) / b : a / b; }
+
+int floor_div(int a, int b) { return a <= 0 ? (a - b + 1) / b : a / b; }
+
+void add_mod(int& a, int b) { a = a + b >= kMod ? a + b - kMod : a + b; }
+
+void minus_mod(int& a, int b) { a = (a - b + kMod) % kMod; }
+
+void mul_mod(int& a, int b) { a = a * b % kMod; }
+
+void maximize(int& x, int y) { x = max(x, y); }
+
+void minimize(int& x, int y) { x = min(x, y); }
+
+int fpow(int a, int b) {
+  int res = 1;
+  while (b) {
+    if (b & 1) res = res * a % kMod;
+    a = a * a % kMod;
+    b >>= 1;
+  }
+  return res;
+}
+
+int gcd(int a, int b) {
+  if (a < b) swap(a, b);
+  return b == 0 ? a : gcd(b, a % b);
+}
+
+void solve() {
+  int n;
+  cin >> n;
+
+  vector<int> a(n);
+  for (int& c : a) cin >> c;
+
+  vector<vector<int>> adj(n, vector<int>());
+  for (int i = 0; i < n; ++i) {
+    int tmp = a[i];
+    while (tmp != 2) {
+      adj[i].push_back(tmp);
+      if (tmp % 2 == 0) {
+        tmp /= 2;
+      } else {
+        tmp++;
+      }
+    }
+    reverse(adj[i].begin(), adj[i].end());
+  }
+
+  int ans = 0;
+  for (auto& i : adj) ans += i.size();
+
+  int minn = *min_element(a.begin(), a.end());
+  if (minn == 1) {
+    int bonus = n;
+
+    for (int i = 0; i < n; ++i) {
+      if (a[i] != 1) continue;
+      bonus -= 2;
+    }
+
+    cout << ans + min(0LL, bonus) << '\n';
+    return;
+  }
+
+  int pos = 0;
+  while (true) {
+    if (adj[0].size() == pos) break;
+    bool ok = true;
+
+    for (int i = 1; i < n; ++i) {
+      if (adj[i].size() == pos) {
+        ok = false;
+        break;
+      }
+
+      if (adj[i][pos] != adj[i - 1][pos]) {
+        ok = false;
+        break;
+      }
+    }
+
+    if (!ok) break;
+    ans -= n;
+    pos++;
+  }
+  cout << ans << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
